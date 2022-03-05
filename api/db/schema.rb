574716455faa004e0f9250e20997e7408bf9e09e
yuta.onishi <rad6242@icloud.com>
2022-03-05 22:20:44 +0900

    chore: ideaモデルとmigrationをgenerate #4
    ideaモデルを作成しました。
    ideasテーブルをmigrationし、必要な制約を与えました。

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_130933) do
+ActiveRecord::Schema.define(version: 2022_03_05_131516) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -19,4 +19,13 @@ ActiveRecord::Schema.define(version: 2022_03_05_130933) do
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
+  create_table "ideas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_ideas_on_category_id"
+  end
+
+  add_foreign_key "ideas", "categories"
 end
