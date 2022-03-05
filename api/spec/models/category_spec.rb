3089a0f5569ee6e09ffd2f094f34274e6252c1d6
yuta.onishi <rad6242@icloud.com>
2022-03-06 02:29:21 +0900

    test: categoryに対するテストを追加
    存在性や保存に対するテストを追加しました。
    rspecの結果出力をformatするように設定を変更しました。

--- api/spec/models/category_spec.rb
+++ api/spec/models/category_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'idea' do
+
+  end
+
+  context '新規のカテゴリーを保存しようとした場合' do
+    it 'nameに値があれば保存できる' do
+      expect(create(:category, :task)).to be_valid
+    end
+
+    it 'nameに値がなければ保存できず例外が発生する' do
+      expect { Category.create(name: '') }.to raise_error
+    end
+
+    it '空白文字では保存できず例外が発生する' do
+      expect { Category.create(name: '     ') }.to raise_error
+    end
+  end
+
+  context '既存のカテゴリーを保存しようとした場合' do
+    before do
+      create(:category, :task)
+    end
+
+    it '保存できず例外が発生する' do
+      existed_task = build(:category, :task)
+      expect { existed_task.save }.to raise_error ActiveRecord::RecordNotUnique
+    end
+
+    # it 'categoriesの件数は変わらない' do
+    # end
+  end
 end
