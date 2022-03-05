cfe271f927eefea1c239cd1861249356557b22f7
yuta.onishi <rad6242@icloud.com>
2022-03-05 22:12:09 +0900

    fix: categoiesテーブルのnameにユニーク制約を付与 fix: categoriesテーブルのnameにユニーク制約を付与 #3
    ユニーク制約を付与して、migrationを実施しました。

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_123628) do
+ActiveRecord::Schema.define(version: 2022_03_05_130933) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
 end
