67a5c36e1a924dbf8af170f387d7e1763bc6f739
yuta.onishi <rad6242@icloud.com>
2022-03-06 17:14:22 +0900

    feat: アイデア登録APIを作成 #8
    仕様に則ってAPIを作成しました。
    登録成功時には201を、失敗時には422を返します。

--- api/app/controllers/api/v1/ideas_controller.rb
+++ api/app/controllers/api/v1/ideas_controller.rb
@@ -2,8 +2,16 @@
 
 module Api
   module V1
-    # アイデア登録API
+    # アイデア取得・登録API
     class IdeasController < ApplicationController
+      def create
+        category = Category.find_or_create_by(name: params[:category_name])
+        if Idea.create!(category_id: category.id, body: params[:body])
+          render status: 201
+        else
+          render status: 422
+        end
+      end
     end
   end
 end
