e5454e281e9892eee2f527111e41c84d8e8bd22d
yuta.onishi <rad6242@icloud.com>
2022-03-06 16:23:45 +0900

    test: アイデア登録APIに対するテストを追加
    request_specでテストを追加しました。
    保存する際に、HTTPステータスコードとテーブルの件数についてテストしています。

--- api/spec/requests/ideas_spec.rb
+++ api/spec/requests/ideas_spec.rb
@@ -1,7 +1,70 @@
 require 'rails_helper'
 
-RSpec.describe "Ideas", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Ideas', type: :request do
+  describe 'アイデア登録API' do
+    context 'リクエストのcategory_nameがcategoriesテーブルのnameに存在する場合' do
+      before do
+        @exist_category = create(:category, :task)
+      end
+
+      it 'ideasテーブルに登録できて、ステータスコード201を返す' do
+        post api_v1_ideas_path, params: {
+          category_name: @exist_category.name,
+          body: build(:idea, :doing_task)
+        }
+        expect(response).to have_http_status(201)
+      end
+
+      it 'ideasテーブルに登録できて、ideasテーブルの件数が増える' do
+        expect do
+          post api_v1_ideas_path, params: {
+            category_name: @exist_category.name,
+            body: build(:idea, :doing_task)
+          }
+        end.to change { Idea.count }.from(0).to(1)
+      end
+
+      it 'ideasテーブルに登録できて、Categoryの件数は変わらない' do
+        expect do
+          post api_v1_ideas_path, params: {
+            category_name: @exist_category.name,
+            body: build(:idea, :doing_task)
+          }
+        end.to change { Category.count }.by(0)
+      end
+    end
+
+    context 'リクエストのcategory_nameがcategoriesテーブルのnameに存在しない場合' do
+      before do
+        @exist_category = create(:category, :task)
+        @new_category = build(:category, :application)
+      end
+
+      it 'ideasテーブルに登録できて、ステータスコード201を返す' do
+        post api_v1_ideas_path, params: {
+          category_name: @new_category.name,
+          body: build(:idea, :doing_task)
+        }
+        expect(response).to have_http_status(201)
+      end
+
+      it 'ideasテーブルに登録できて、ideasテーブルの件数が増える' do
+        expect do
+          post api_v1_ideas_path, params: {
+            category_name: @new_category.name,
+            body: build(:idea, :doing_task)
+          }
+        end.to change { Idea.count }.from(0).to(1)
+      end
+
+      it 'ideasテーブルに登録でき、Categoryの件数も増加する' do
+        expect do
+          post api_v1_ideas_path, params: {
+            category_name: @new_category.name,
+            body: build(:idea, :doing_task)
+          }
+        end.to change { Category.count }.from(1).to(2)
+      end
+    end
   end
 end
